8376f02ed2f51e279a2d23b76db2caac7177fd89
Flonicx-Lab <flonicx22@gmail.com>
2026-09-02 13:55:59 +0900

    refactor : 플레이어 이동, 스피드변경 함수 추출

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -5,62 +5,67 @@ public class PlayerMove : MonoBehaviour
     // 목적 : 키보드 입력에 따라서 플레이어 이동 처리
     // 필요 필드:
     public float Speed;
-    private float leftBoundary = -2.4f;
-    private float rightBoundary = 2.4f;
-    private float upBoundary = -0.6f;
-    private float downBoundary = -5.2f;
+    
+    public float leftBoundary;
+    public float rightBoundary;
+    public float upBoundary;
+    public float downBoundary;
 
     private void SpeedUp()
     {
-        Speed += 0.1f;
+        Speed ++;
     }
 
     private void SpeedDown()
     {
-        Speed -= 0.1f;
+        Speed ++;
     }
-    
+
+    public void Replay()
+    {
+        
+    }
+
+    public void Record()
+    {
+        
+    }
     
     // 매 프레임마다 실행된다.
     // 초당 프레임 실행 횟수는 별다른 설정이 없을 경우 가능한 많이
     private void Update()
     {
-        // 1. 키보드 입력을 받는다.
-        float h = Input.GetAxisRaw("Horizontal"); // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f 
-        float v = Input.GetAxisRaw("Vertical"); // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f //Raw 는 더 빠르게 
-        
-        Debug.Log($"h:{h}, v:{v}");
-        
-        // 2. 키보드 입력에 따라 방향을 구한다.
-        // 게임에는 벡터라는 타입이 있다. 벡터는(크기와 방향을 의미한다.)
-        Vector2 direction = new Vector2(h, v);
-        Vector2 normalizedDirection = direction.normalized;
-        
-        // 3. 방향과 속도에 따라 이동한다.
-        // 속도 = 방향 * 속력                        // 매직 넘버란: 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자
+        Move();
+        SpeedChange();
+    }
 
-        Vector2 speed = normalizedDirection * Speed; // 벡터 길이를 1로 만들어주는 것(방향은 유지)
-        
-        //transform.Translate(normalizedSpeed * Time.deltaTime);
-        // deltaTime : 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환 (시간 / 프레임)
-        
-        // 새로운 위치 = 현재 위치 + v * t
-        // transform.position +=  (Vector2)direction* normalizedSpeed * Time.deltaTime);
-
-        Vector2 nextPosition = (Vector2)transform.position + speed * Time.deltaTime;
-
-        
-        // 스피드업 다운
-        if (Input.GetKey(KeyCode.E))
+    private void SpeedChange()
+    {
+        // 스피드업 다운 (E/Q)
+        if (Input.GetKeyDown(KeyCode.E))
         {
             SpeedUp();
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             SpeedDown();
         }
+    }
+
+    private void Move()
+    {
+        // 1. 키보드 입력을 받는다.
+        float h = Input.GetAxisRaw("Horizontal"); // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f 
+        float v = Input.GetAxisRaw("Vertical"); // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f //Raw 는 더 빠르게 
         
+        // 2. 키보드 입력에 따라 방향을 구한다.
+        Vector2 direction = new Vector2(h, v);
+        Vector2 normalizedDirection = direction.normalized;
+        
+        // 3. 방향과 속도에 따라 이동한다.
+        Vector2 speed = normalizedDirection * Speed;
+        Vector2 nextPosition = (Vector2)transform.position + speed * Time.deltaTime;
         
         // 좌 우 경계에서 등장
         if (nextPosition.x > rightBoundary)
@@ -73,17 +78,10 @@ public class PlayerMove : MonoBehaviour
         }
         
         // 위 아래 막아두기
-        if (nextPosition.y >= downBoundary
+        if (nextPosition.y > downBoundary
             && nextPosition.y < upBoundary)
         {
             transform.position = nextPosition;
         }
-        
-        
-
-
-
     }
-    
-    
 }

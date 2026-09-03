f626c0b114da58324de67af765c3dc27143b3d29
Flonicx-Lab <flonicx22@gmail.com>
2026-09-03 12:56:30 +0900

    feat: 총알 충돌 구현

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -2,12 +2,36 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float Speed;
+    public float MoveSpeed;
 
 
     private void Update()
     {
         Vector2 direction = Vector2.up; // new Vector2(1,0);
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+    }
+
+    //충돌 관련 이벤트 (Enter -> Stay -> Exit)
+
+    //충돌이 시작되면 호출되는 이벤트 함수
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("충돌 했다!");
+
+        // 나 죽고!
+        Destroy(this.gameObject);
+
+        // 너 죽자!
+        Destroy(collision.gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("충돌 중이다!");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("충돌 끝났다!");
     }
 }

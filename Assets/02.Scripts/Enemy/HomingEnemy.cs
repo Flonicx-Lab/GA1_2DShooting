b4a2de2586f2a3cafb04ea7f0c213b0d7d6256e7
Flonicx-Lab <flonicx22@gmail.com>
2026-09-04 10:36:24 +0900

    test: 깃허브 액션 테스트

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -6,7 +6,6 @@ public class HomingEnemy : Enemy
 
     private void Start()
     {
-        _player = GameObject.FindWithTag("Player");
     }
 
     private void Update()
@@ -16,6 +15,7 @@ public class HomingEnemy : Enemy
 
     protected override void Move()
     {
+        _player = GameObject.FindWithTag("Player");
         // 방향 설정
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();

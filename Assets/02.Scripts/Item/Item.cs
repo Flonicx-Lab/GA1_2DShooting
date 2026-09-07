2796870fc050754f49a7785a2816cd9f4cd1cb58
Flonicx-Lab <flonicx22@gmail.com>
2026-09-07 10:06:07 +0900

    feat: 아이템이 멈춰있다가 플레이어에게 이동,흡수

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -1,5 +1,40 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] float _itemSpeed;
+    [SerializeField] float _itemFreezeTime;
+    protected GameObject _player;
+    private float _timer = 0f;
+
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        ItemMove();
+    }
+
+    protected void ItemMove()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _itemFreezeTime)
+        {
+            if (_player == null) return;
+            Vector2 direction = (_player.transform.position - transform.position).normalized;
+            transform.Translate(direction * _itemSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }

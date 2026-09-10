f68cf046c0d0801fa809182937b442de9d8b9556
Flonicx-Lab <flonicx22@gmail.com>
2026-09-10 11:07:09 +0900

    feat: (수업) 오토무브 트래킹 정지 추가

--- Assets/02.Scripts/Player/ClassPlayerAutoMove.cs
+++ Assets/02.Scripts/Player/ClassPlayerAutoMove.cs
@@ -1,15 +1,17 @@
+using UnityEditor.SpeedTree.Importer;
 using UnityEngine;
 
 public class ClassPlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    private GameObject _target = null;
+    [SerializeField] private GameObject _target = null;
+    [SerializeField] private float _stopTrackingY = -2;
 
 
     private void Update()
     {
-        if (_target == null)
+        if (_target == null || _target.transform.position.y < _stopTrackingY)
         {
             FindNearestTarget();
         }
@@ -22,9 +24,22 @@ public class ClassPlayerAutoMove : MonoBehaviour
         if (_target == null) return;
 
         // 2. 방향을 구한다.
-        Vector3 direction = _target.transform.position - transform.position;
+        Vector3 diff = _target.transform.position - transform.position;
+        Vector3 direction = diff;
+
+        // 적과 나와의 y축 차이가 3보다 크면 앞으로 가고 아니면 뒤로가게
+
+        if (diff.y >= 3)
+        {
+            direction.y = 1;
+        }
+        else
+        {
+            direction.y = -1;
+        }
+
         direction.Normalize();
-        direction.y = 0;
+
 
         // 3. 속도에 맞게 이동을 한다.
         transform.position += direction * _speed * Time.deltaTime;
@@ -36,18 +51,23 @@ public class ClassPlayerAutoMove : MonoBehaviour
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
         if (targets.Length == 0) return;
 
-        GameObject target = targets[0];
+        _target = targets[0];
         float minDistance = float.MaxValue;
 
         foreach (GameObject enemy in targets)
         {
+            if (enemy.transform.position.y < _stopTrackingY)
+            {
+                continue;
+            }
+
             //거리를 구함
             float distance = Vector2.Distance(transform.position, enemy.transform.position);
             if (distance < minDistance) // 저장된 거리보다 짧다면
             {
                 //타겟 변경
                 minDistance = distance;
-                target = enemy;
+                _target = enemy;
             }
         }
     }

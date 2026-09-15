e18c2eaa173d10bab43fe674655343c7003ab6ef
Flonicx-Lab <flonicx22@gmail.com>
2026-09-15 11:10:54 +0900

    feat: 업그레이드 json 저장/로드
    - UpgradeSaveData - ToJson, FromJson

--- Assets/02.Scripts/Upgrade/UpgradeManager.cs
+++ Assets/02.Scripts/Upgrade/UpgradeManager.cs
@@ -13,6 +13,7 @@ public class UpgradeManager : MonoBehaviour
 
     // 업그레이드 UI들
     [SerializeField] private UI_Upgrade[] _uiUpgrades;
+    private const string UpgradeSaveDataKey = "UpgradeSaveData";
 
     private void Awake()
     {
@@ -66,16 +67,33 @@ public class UpgradeManager : MonoBehaviour
     {
         // 데이터 저장은 유의미한 정보만 저장한다.
         // 그래서 레벨만 저장
+        UpgradeSaveData saveData = new UpgradeSaveData(_upgrades.Length);
         for (int i = 0; i < _upgrades.Length; i++)
         {
-            int level = PlayerPrefs.GetInt($"Upgrade.{i}Level", 1);
-            _upgrades[i].SetLevel(level);
+            saveData.Name[i] = _upgrades[i].Name;
+            saveData.Level[i] = _upgrades[i].Level;
         }
 
+        // json 포멧 문자열 변환
+        // 키와 밸류 형태로 저장한 형태
+
+        string json = JsonUtility.ToJson(saveData);
+
+        PlayerPrefs.SetString(UpgradeSaveDataKey, json);
         PlayerPrefs.Save();
     }
 
     private void Load()
     {
+        if (!PlayerPrefs.HasKey(UpgradeSaveDataKey)) return;
+
+        string json = PlayerPrefs.GetString(UpgradeSaveDataKey, string.Empty);
+        UpgradeSaveData saveData = JsonUtility.FromJson<UpgradeSaveData>(json);
+
+        for (int i = 0; i < _upgrades.Length; i++)
+        {
+            Debug.Log($"{_upgrades[i].Name} 로드 완료!");
+            _upgrades[i].SetLevel(saveData.Level[i]);
+        }
     }
 }
